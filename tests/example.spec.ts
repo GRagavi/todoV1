7ae581bf49294f93cbb10617e3991c8fa4be1d2d
GRagavi <sharmi9318@gmail.com>
2023-02-09 19:26:12 +0530

    second commit

--- tests/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('First Test', async ( ) => {
-  expect(5+6).toBe(11)
-});
-
-
---
+++ util/todo.ts
@@ -0,0 +1,18 @@
+import { APIRequestContext } from "@playwright/test";
+import { json } from "stream/consumers";
+
+export async function createTodo(request:APIRequestContext,body:{title:string,status?:string}){
+    
+    const resp = await request.post("/v1/todo",{
+       data: body,
+       headers:{
+        'content-Type': 'application/json'
+       }
+    })
+    return {status:resp.status(),body: await resp.json()}
+}
+
+export async function deleteTodo(request:APIRequestContext,id:number){
+    const respons = await request.delete(`/v1/todo/${id}`)
+    return await respons.status()
+}
