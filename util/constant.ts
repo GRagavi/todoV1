1032ce5c7098a286055fe990a93e3b140725a024
Vanmathi123 <124774103+Vanmathi123@users.noreply.github.com>
2023-02-21 09:33:44 +0530

    Update testcases done (#6)

---
+++ util/constant.ts
@@ -0,0 +1,6 @@
+export const createText="Created by Vanmathi";
+export const createStatus="ACTIVE";
+export const updateText ="Updating title by vanmathi via patch";
+export const updateValidstatus:string= "DONE";
+export const updateInvalidstatus:string= "done";
+export const updatePutText ="Updating title by vanmathi via put";
--- util/todo.ts
+++ util/todo.ts
@@ -1,18 +1,38 @@
 import { APIRequestContext } from "@playwright/test";
-import { json } from "stream/consumers";
 
-export async function createTodo(request:APIRequestContext,body:{title:string,status?:string}){
-    
-    const resp = await request.post("/v1/todo",{
-       data: body,
-       headers:{
-        'content-Type': 'application/json'
-       }
-    })
-    return {status:resp.status(),body: await resp.json()}
+export async function getToDo(request: APIRequestContext, id) {
+    const getresponse = await request.get(`http://144.24.105.148:8080/v1/todo/${id}`);
+    return (await getresponse.json());
 }
 
-export async function deleteTodo(request:APIRequestContext,id:number){
+export async function createTodo(request: APIRequestContext, body: { title: string, status?: string }) {
+
+    const resp = await request.post("/v1/todo", {
+        data: body,
+        headers: {
+            'content-Type': 'application/json'
+        }
+    })
+    return { status: resp.status(), body: await resp.json() }
+}
+
+export async function updateToDo(request: APIRequestContext, id, body: { title?: string, status?: String }) {
+    const updateresponse = await request.patch(`/v1/todo/${id}`, {
+        data: body,
+        headers: { 'Content-Type': 'application/json' }
+    })
+    return { status: updateresponse.status(), body: await updateresponse.json() };
+}
+
+export async function putToDo(request: APIRequestContext, id, body: { title?: string, status?: String }) {
+    const putresponse = await request.put(`/v1/todo/${id}`, {
+        data: body,
+        headers: { 'Content-Type': 'application/json' }
+    })
+    return { status: putresponse.status(), body: await putresponse.json() };
+}
+
+export async function deleteTodo(request: APIRequestContext, id: number) {
     const respons = await request.delete(`/v1/todo/${id}`)
     return await respons.status()
 }
